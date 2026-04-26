0f42e409905c9ec3e9fbd7a3fa8f37d4bd906dc2
Adiel Calixto <adielcalixtos@outlook.com>
2026-04-26 15:38:31 -0300

    chore: update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "2.1.0"
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "com.adielcalixto.ifacademico.CustomTestRunner"
     }
